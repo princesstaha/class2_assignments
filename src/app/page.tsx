dbfd6df4f3d177bad92f412da559f44977ab7d9c
princesstaha <159701680+princesstaha@users.noreply.github.com>
2024-10-01 19:34:01 +0500

    assignment

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    < div className = "parentContainer">
+      {/* Below Code Is With CSS */}
+      <div className="homeContainer">
+        <div className="childContainer">
+          Hey,
+          <br />
+          My Name is <span className="pinkColor">Taha</span>
+          <br />I will become a Next JS Developer
+        </div>
+        <div className="childContainer"></div>
+      </div>
+
+      </div>
+   
+  );
+}
